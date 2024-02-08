ae9ba6e3ff5ee2e9e614505d3f627d20b7697573
p541i <p.joshi@kitz-heidelberg.de>
2024-02-08 21:36:07 +0100

    changed file names and modified scripts

--- RNA/002_intialprocessingRNA.R
+++ RNA/002_intialprocessingRNA.R
@@ -1,5 +1,8 @@
-## This script uses Gene and GeneFull counts from STAR aligner to generate a combined # This version includes SoupX on uncorrected counts. 
-# SoupX is done on fixed counts, which includes preMRNA + exons counts of overlapping/undercounted genes
+## This script uses GeneFull (intronic) and Gene (exonic) counts from STAR aligner to generate a combined gene matrix
+## The combined gene matrix is used by diem to identify cell vs debris.
+## The output of diem is used by SoupX and decontX to remove debris.
+## The final output is saved 
+## SoupX is done on fixed counts, which includes preMRNA + exons counts of overlapping/undercounted genes
 # 1. Data input----
 suppressPackageStartupMessages({
   library(diem)
@@ -11,15 +14,14 @@ suppressPackageStartupMessages({
 })
 
 #argument
-args = commandArgs(trailingOnly=TRUE)
+args = commandArgs(trailingOnly=TRUE) ##Sample id
 
 ## on server----
 #Load the premrna counts
-DIR="~/DATA/kons_analysis/indana/MBnew/"
+DIR="~/path/to/rawcounts/"
 counts.in <- read_10x(paste0(DIR,args[1],"/GeneFull"))
 dim(counts.in)
 # load the exon part
-
 counts.ex <- read_10x(paste0(DIR,args[1],"/Gene"))
 dim(counts.ex)
 
@@ -39,22 +41,6 @@ mdfr <- mdfr[order(-mdfr$tot),]
 mdfr$Rank <- seq(1:dim(mdfr)[1])
 ##subsetting 20k cells
 mdfr2 <- mdfr[1:20000,]
-# 
-## not run -----
-# ##mclust to find cells based on fraction plot
-# cl <- Mclust(mdfr2[,c("frac")], G=2)
-# #finding'class' of cells
-# d <- table(factor(cl$classification))
-# k <- as.integer(which(d==min(d)))
-# #knee point as posisble number of cells
-# 
-# #finding cells from probability (z)
-# keep <- cl$z[,k]>=0.95
-# table(keep)
-# kp <- as.integer(table(keep)[2])
-#
-#find similar cells----
-
 rm(mdfr, intronic, exonic, tot, frac)
 
 #check colnames
@@ -79,12 +65,37 @@ counts.in2 <- rbind(counts.in2,counts.ex2)
 dim(counts.in2)
 counts.in2 <- counts.in2[genes,]
 rm(genes)
+
+#check colnames and rownames
+if(all(colnames(counts.in) ==colnames(counts.ex)) & all(row.names(counts.in) == row.names(counts.ex))){
+  
+  #head(counts.ex)[,1:10]
+  genes <- read.delim("~/HB/ann/gencdH38p13r37CR_genesN.txt", header = FALSE) ##ENS id
+  genes <- unique(genes$V1)
+  
+  #finding genes that were counted less with intronic marix
+  mean.in <- rowMeans(counts.in)
+  mean.ex <- rowMeans(counts.ex)
+  d <- (mean.ex > mean.in)
+  print(table(d))
+  #replacing above genes' data in intronic matrix from exonic matrix
+  counts.in2 <- counts.in[!d,]
+  dim(counts.in2)
+  counts.ex2 <- counts.ex[d,]
+  dim(counts.ex2)
+  
+  counts.in2 <- rbind(counts.in2,counts.ex2)
+  dim(counts.in2)
+  counts.in2 <- counts.in2[genes,]
+  rm(genes)
+}
+
 ## 1.1 Creating an SCE object------
 
 mdt <- create_SCE(counts.in2, name=args[1])
 dim(mdt)
 #class(mdt)
-### remoe all count matrices
+### remove all count matrices
 rm(counts.ex2, counts.ex, counts.in)
 #processing using diem
 
@@ -104,16 +115,13 @@ mdt <- get_gene_pct(x = mdt, genes = genes, name = "MALAT1")
 
 # The following outlines the steps involved in `diem`:
 #
-#   1. Speciyfing the test set and the debris set.
+# 1. Speciyfing the test set and the debris set.
 # 2. Running PCA on test set
 # 2. Running k-means on the PCs to initialize the clusters.
-# 4. Running a EM to estimate the parameters of the Dirichlet-multinomial
-# mixture model.
+# 4. Running a EM to estimate the parameters of the Dirichlet-multinomial mixture model.
 # 5. Classifying droplets based on their likelihood.
 
 ## 2.1 Specifying test and debris droplets
-
-
 mdt <- set_debris_test_set(mdt, top_n = 15000,
                            min_counts = 200,
                            min_genes = 200)
@@ -146,10 +154,6 @@ mdt <- init(mdt,
             seedn = 123,
             threads = 8)
 
-#drop_data <- droplet_data(mdt)
-# table(drop_data[,"Cluster"])
-# tapply(drop_data[,"n_genes"],drop_data[,"Cluster"], mean)
-# tapply(drop_data[,"pct.mt"], drop_data[,"Cluster"], mean)
 ## 3.4 Running EM using DM like before-----
 mdt <- run_em(mdt, threads=8)
 mdt <- assign_clusters(mdt)
@@ -158,25 +162,25 @@ mdt <- estimate_dbr_score(mdt)
 
 #de_genes <- debris_genes(mdt)
 #head(de_genes)
-mdt <- call_targets(mdt,thresh_score = .5)
+mdt <- call_targets(mdt,thresh_score = .5, min_genes = 200)##
 mdt.pd <- droplet_data(mdt)
 colnames(mdt.pd)[1:2] <- c("nUMIs", "nGenes")
 mdt.pd <- mdt.pd[order(-mdt.pd$nUMIs),]
 mdt.pd$Rank <- seq(dim(mdt.pd)[1])
-mdt.pd$nUMIs <- mdt.pd$nUMIs+1
+mdt.pd$nUMIs <- mdt.pd$nUMIs+1 ##to account for zero
 
 # save ----
 save(mdt, mdfr2,mdt.pd, file=paste0("diemv4",args[1],".RData"))
 #load(paste0("diemv4",args[1],".RData"))
-cells <- row.names(mdt.pd[mdt.pd$Call=="Clean",])
-sce <- SingleCellExperiment(list(counts=mdt@counts[,cells]))
+cells <- row.names(mdt.pd[mdt.pd$Call=="Clean",]) ##filter for "Clean" cells
+sce <- SingleCellExperiment(list(counts=mdt@counts[,cells])) ##conver to SCE object
 #table(row.names(drop_data)==colnames(sce))
 del <- mdt.pd[colnames(sce),]
 colData(sce) <- DataFrame(del)
 
 mdfr3 <- mdfr2[colnames(sce),]
 sce$in_ex_frac <- mdfr3$frac
-
+##add sample id to cell names
 row.names(mdt.pd) <- paste0(args[1],"_",row.names(mdt.pd))
 colnames(sce) <- paste0(args[1],"_",colnames(sce))
 
@@ -189,12 +193,12 @@ if (z <2) z =2
 ##based on previous data 6600 genes are in 98% quantile
 keep <- (sce$pct.mt < z) & (sce$nGenes < 6600)
 sce <- sce[,keep]
-keep <- sce$in_ex_frac >0.25
+keep <- sce$in_ex_frac >0.25 #intronic fraction more than 25%
 sce <- sce[,keep]
 
 rm(x,keep,z, mdt, mdfr3)
 
-#removing soup----
+#removing soup using SoupX----
 suppressPackageStartupMessages({
   library(SoupX)
   library(Seurat)
@@ -202,7 +206,7 @@ suppressPackageStartupMessages({
 })
 
 #to select important debris genes
-#find custers and UMAP
+#Converting into Seurat object for running SoupX
 sce.S <- CreateSeuratObject(counts = counts(sce))
 sce.S <- SCTransform(sce.S, verbose = FALSE)
 sce.S <- RunPCA(sce.S)
@@ -211,7 +215,7 @@ sce.S <- FindNeighbors(sce.S, dims = 1:30)
 sce.S <- FindClusters(sce.S, resolution = 1)
 
 filt.matrix <- counts(sce)
-soup.channel  <- SoupChannel(counts.in2, filt.matrix)
+soup.channel  <- SoupChannel(counts.in2, filt.matrix) ##counts.in2 is the fixed intronic gene expression matrix, line 64
 rm(counts.in2)
 meta    <- sce.S@meta.data
 umap    <- sce.S@reductions$umap@cell.embeddings
@@ -238,14 +242,14 @@ soupx_corrected  <- adjustCounts(soup.channel, roundToInt = T)
 assay(sce,"unfiltcounts") <- counts(sce)
 counts(sce) <- soupx_corrected
 
+##running decontX
 library(celda)
-#deconTX
 sce <- decontX(sce, z = sce.S$seurat_clusters)
 assay(sce,"rawcounts") <- counts(sce)
 counts(sce) <-  decontXcounts(sce)
 decontXcounts(sce) <- NULL
 
-save(sce,mdfr2,mdt.pd, file = paste0(args[1],"_postSoupXDX.RData"))
+save(sce,mdfr2,mdt.pd, file = paste0(args[1],"_postSoupXDX.RData")) ##This will be used to remove doublets
 
 q()
 
